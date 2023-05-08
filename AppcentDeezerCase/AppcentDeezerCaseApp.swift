8a451d3297beca22ddccf4dd1c99924384466816
Kaan Uzman <55639112+akaanuzman@users.noreply.github.com>
2023-05-08 15:41:08 +0300

    init commit

---
+++ AppcentDeezerCase/AppcentDeezerCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppcentDeezerCaseApp.swift
+//  AppcentDeezerCase
+//
+//  Created by Kaan Uzman on 8.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppcentDeezerCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
